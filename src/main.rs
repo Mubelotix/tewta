08be33cf382cd9fcd9210a7b78b0e7c6cdbfe0d6
Mubelotix <mubelotix@gmail.com>
2022-03-10 17:51:14 +0100

    Add AsyncWrite

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::{sync::Arc, io::Write, future::Future, task::Poll, pin::Pin};
 use log::*;
 use futures::{future::BoxFuture, FutureExt};
 use async_mutex::Mutex;
+use tokio::io::{AsyncRead, AsyncWrite};
 
 mod commands;
 use commands::*;
-use tokio::io::AsyncRead;
 
 static mut RUNNING_COMMAND_COUNTER: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
 
@@ -14,6 +14,7 @@ pub struct TestStream {
     outbound: Arc<Mutex<Vec<u8>>>,
 
     inbound_lock_fut: Option<BoxFuture<'static, async_mutex::MutexGuardArc<Vec<u8>>>>,
+    outbound_lock_fut: Option<BoxFuture<'static, async_mutex::MutexGuardArc<Vec<u8>>>>,
 }
 
 impl TestStream {
@@ -25,12 +26,14 @@ impl TestStream {
             TestStream {
                 inbound: inbound.clone(),
                 outbound: outbound.clone(),
-                inbound_lock_fut: None
+                inbound_lock_fut: None,
+                outbound_lock_fut: None,
             },
             TestStream {
                 inbound: outbound,
                 outbound: inbound,
-                inbound_lock_fut: None
+                inbound_lock_fut: None,
+                outbound_lock_fut: None,
             },
         )
     }
@@ -60,6 +63,34 @@ impl AsyncRead for TestStream {
         } else {
             Poll::Pending
         }
+    }
+}
+
+impl AsyncWrite for TestStream {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        if self.outbound_lock_fut.is_none() {
+            let self_outbound = Arc::clone(&self.outbound);
+            self.outbound_lock_fut = Some(async move { self_outbound.lock_arc().await }.boxed());
+        }
+
+        if let Poll::Ready(mut outbound) = self.outbound_lock_fut.as_mut().unwrap().as_mut().poll(cx) {
+            outbound.extend_from_slice(buf);
+            Poll::Ready(Ok(buf.len()))
+        } else {
+            Poll::Pending
+        }
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut std::task::Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        unimplemented!("Shutdown on virtual testing streams is not implemented");
     }
 }
 
