9d28a1b98aadf1aa6eb0795e82c685bf13da37cc
Mubelotix <mubelotix@gmail.com>
2022-03-29 13:24:18 +0200

    Add const generic on segmented array

--- src/segmented_array.rs
+++ src/segmented_array.rs
@@ -5,26 +5,26 @@ use crate::prelude::*;
 use sha2::{Sha256, Digest};
 
 #[derive(Debug, Clone)]
-pub enum Segment<T: Hash + std::fmt::Debug> {
+pub enum Segment<T: Hash + std::fmt::Debug, const N: usize> {
     Item(T),
-    SegmentedArray(SegmentedArray<T>),
+    SegmentedArray(SegmentedArray<T, N>),
     Unknown(Box<[u8; 32]>),
 }
 
 /// An array data type whose items can be removed without affecting its hash.
 /// Note: order of items must not change.
 #[derive(Debug, Clone)]
-pub struct SegmentedArray<T: Hash + std::fmt::Debug> {
-    pub segments: Vec<Segment<T>>,
+pub struct SegmentedArray<T: Hash + std::fmt::Debug, const N: usize> {
+    pub segments: Vec<Segment<T, N>>,
 }
 
-impl<T: Hash + Clone + std::fmt::Debug> SegmentedArray<T> {
+impl<T: Hash + Clone + std::fmt::Debug, const N: usize> SegmentedArray<T, N> {
     pub fn remove_segment(&mut self, i: usize) {
         self.segments[i] = Segment::Unknown(self.segments[i].hash());
     }
 }
 
-impl<T: Hash + Clone + std::fmt::Debug> Segment<T> {
+impl<T: Hash + Clone + std::fmt::Debug, const N: usize> Segment<T, N> {
     fn items(self) -> Vec<T> {
         match self {
             Segment::Item(item) => vec![item],
@@ -34,7 +34,7 @@ impl<T: Hash + Clone + std::fmt::Debug> Segment<T> {
     }
 }
 
-impl<T: Hash + Clone + std::fmt::Debug> SegmentedArray<T> {
+impl<T: Hash + Clone + std::fmt::Debug, const N: usize> SegmentedArray<T, N> {
     fn items(self) -> Vec<T> {
         let mut items = Vec::new();
         for segment in self.segments {
@@ -44,31 +44,31 @@ impl<T: Hash + Clone + std::fmt::Debug> SegmentedArray<T> {
     }
 }
 
-impl<T: Hash + Clone + std::fmt::Debug> From<SegmentedArray<T>> for Vec<T> {
-    fn from(val: SegmentedArray<T>) -> Self {
+impl<T: Hash + Clone + std::fmt::Debug, const N: usize> From<SegmentedArray<T, N>> for Vec<T> {
+    fn from(val: SegmentedArray<T, N>) -> Self {
         val.items()
     }
 }
 
-impl<T: Hash + Clone + std::fmt::Debug> From<Vec<T>> for SegmentedArray<T> {
+impl<T: Hash + Clone + std::fmt::Debug, const N: usize> From<Vec<T>> for SegmentedArray<T, N> {
     fn from(items: Vec<T>) -> Self {
-        let mut segments: Vec<Segment<T>> = Vec::new();
+        let mut segments: Vec<Segment<T, N>> = Vec::new();
         for item in items {
             segments.push(Segment::Item(item));
         }
 
         loop {
-            if segments.len() <= 16 {
+            if segments.len() <= N {
                 return SegmentedArray { segments };
             }
 
             let mut segments_iter = segments.into_iter();
-            let mut new_segments: Vec<Segment<T>> = Vec::new();
+            let mut new_segments: Vec<Segment<T, N>> = Vec::new();
             
             while let Some(segment) = segments_iter.next() {
-                let mut new_segmented_array = Vec::with_capacity(16);
+                let mut new_segmented_array = Vec::with_capacity(N);
                 new_segmented_array.push(segment);
-                for _ in 0..15 { // It's 15 and not 16 because we already have one item
+                for _ in 0..N-1 { // It's N-1 because we already have one item
                     match segments_iter.next() {
                         Some(segment) => new_segmented_array.push(segment),
                         None => break,
@@ -83,7 +83,7 @@ impl<T: Hash + Clone + std::fmt::Debug> From<Vec<T>> for SegmentedArray<T> {
     }
 }
 
-impl<T: Hash + std::fmt::Debug> Hash for Segment<T> {
+impl<T: Hash + std::fmt::Debug, const N: usize> Hash for Segment<T, N> {
     fn hash(&self) -> Box<[u8; 32]> {
         match self {
             Segment::Item(item) => item.hash(),
@@ -93,7 +93,7 @@ impl<T: Hash + std::fmt::Debug> Hash for Segment<T> {
     }
 }
 
-impl<T: Hash + std::fmt::Debug> Hash for SegmentedArray<T> {
+impl<T: Hash + std::fmt::Debug, const N: usize> Hash for SegmentedArray<T, N> {
     fn hash(&self) -> Box<[u8; 32]> {
         let mut hasher = Sha256::new();
         for segment in self.segments.iter() {
@@ -119,7 +119,7 @@ mod tests {
             array.push(i);
         }
 
-        let seg_array = SegmentedArray::from(array.clone());
+        let seg_array: SegmentedArray<u16, 16> = SegmentedArray::from(array.clone());
         let array2 = Vec::from(seg_array);
 
         assert_eq!(array, array2);
@@ -132,7 +132,7 @@ mod tests {
             array.push(i);
         }
 
-        let mut seg_array = SegmentedArray::from(array.clone());
+        let mut seg_array: SegmentedArray<u16, 16> = SegmentedArray::from(array.clone());
         let seg_array2 = seg_array.clone();
 
         seg_array.remove_segment(7);
@@ -145,9 +145,12 @@ mod tests {
 
     #[test]
     fn segment_sizes() {
-        fn check_segment_size<T: Hash + std::fmt::Debug>(segment: &Segment<T>) {
+        fn check_segment_size<T: Hash + std::fmt::Debug, const N: usize>(segment: &Segment<T, N>) {
             if let Segment::SegmentedArray(array) = segment {
-                assert!(array.segments.len() <= 16);
+                assert!(array.segments.len() <= N);
+                for segment in &array.segments {
+                    check_segment_size(segment);
+                }
             }
         }
 
@@ -156,7 +159,7 @@ mod tests {
             array.push(i);
         }
 
-        let mut seg_array = SegmentedArray::from(array.clone());
+        let mut seg_array: SegmentedArray<u16, 16> = SegmentedArray::from(array.clone());
 
         for segment in seg_array.segments.iter_mut() {
             check_segment_size(segment);
